364377c74202ba5098fc1b528a11cf81335759dd
Diego Martinez <damartinezvargas@cegid.com>
2021-02-01 08:57:41 +0100

    Add react-output-target

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'designsystemdiego',
+
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'designsystemdiego',
+      proxiesFile: '../designsystemdiego-react/src/components.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
